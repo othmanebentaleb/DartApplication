d339c389b04720adb337e049100a1c93d43cebed
unknown <othmanebentaleb90@gmail.com>
2019-01-01 13:21:56 +0100

    First Dart application

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:helloDartApplication/helloDartApplication.dart' as helloDartApplication;
+
+main(List<String> arguments) {
+  print('Hello world: ${helloDartApplication.calculate()}!');
+}
